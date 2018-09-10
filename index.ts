6b68831cc1a1d5a6ac1431973e1cdfe9727c58b9
Vincent <projects@vinnl.nl>
2018-09-10 11:53:37 +0200

    Add a simple HTTP server using Molen

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log('Testing, testing, 1 2 3');
+import { map } from 'rxjs/operators';
+import * as Http from 'molen/dist/drivers/http';
+
+const port = process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 3000;
+
+const server: Http.HttpHandler = (input$) => {
+  return input$.pipe(
+    map(() => ({ body: [ 'Hi!' ] })),
+  );
+};
+
+Http.driver(server, { port });
