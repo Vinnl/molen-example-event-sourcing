67ce924b87d1a8fa393c1b898858f815146470b5
Vincent <projects@vinnl.nl>
2018-09-10 13:53:54 +0200

    Expose a WebSocket server

--- index.ts
+++ index.ts
@@ -1,12 +1,20 @@
 import { map } from 'rxjs/operators';
 import * as Http from 'molen/dist/drivers/http';
+import * as WebSocket from 'molen/dist/drivers/websocket';
 
 const port = process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 3000;
 
-const server: Http.HttpHandler = (input$) => {
+const app: Http.HttpHandler = (input$) => {
   return input$.pipe(
     map(() => ({ body: [ 'Hi!' ] })),
   );
 };
+const httpServer = Http.driver(app, { port });
 
-Http.driver(server, { port });
+const wsApp: WebSocket.WebSocketHandler = (input$) => {
+  return input$.pipe(
+    map((input) => ({ }))
+  );
+};
+
+WebSocket.driver(wsApp, { server: httpServer });
