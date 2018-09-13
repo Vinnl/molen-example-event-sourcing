5853fc088adf8a1a429e09e13cc79d1ecce572ec
Vincent <projects@vinnl.nl>
2018-09-13 14:33:23 +0200

    Display 5 most recent messages at first

--- src/database/messages.ts
+++ src/database/messages.ts
@@ -5,14 +5,14 @@ import { tap } from "rxjs/operators";
 
 export const getLatestMessages = () => {
   const connection = getConnection();
-  
+
   const latestMessagesPromise = connection
     .getRepository(Message)
     .createQueryBuilder('message')
     .orderBy('message.updated_at', 'DESC')
     .limit(5)
     .getMany();
-    
+
   const latestMessages$ = from(latestMessagesPromise);
 
   return latestMessages$;
